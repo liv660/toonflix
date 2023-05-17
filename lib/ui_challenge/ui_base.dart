f744e7973c9ea740caccf6420003f2eb3f0a1d23
sysim <liv660n@gmail.com>
2023-05-17 16:54:14 +0900

    ui - const and settings

--- lib/ui_challenge/ui_base.dart
+++ lib/ui_challenge/ui_base.dart
@@ -5,20 +5,24 @@ class UiBaseApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xff181818),
+          backgroundColor: const Color(0xff181818),
           body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40,),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 80,),
+                const SizedBox(
+                  height: 80,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Text(
+                        const Text(
                           'Hey, Selena',
                           style: TextStyle(
                             color: Colors.white,
@@ -37,24 +41,30 @@ class UiBaseApp extends StatelessWidget {
                     )
                   ],
                 ),
-                SizedBox(height: 120,),
+                const SizedBox(
+                  height: 120,
+                ),
                 Text(
-                    'Total Balance',
-                    style: TextStyle(
+                  'Total Balance',
+                  style: TextStyle(
                     fontSize: 22,
                     color: Colors.white.withOpacity(0.8),
                   ),
                 ),
-                SizedBox(height: 5,),
-                Text(
-                    '\$5 194 482',
-                    style: TextStyle(
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  '\$5 194 482',
+                  style: TextStyle(
                     fontSize: 48,
-                    fontWeight: FontWeight.w600 ,
+                    fontWeight: FontWeight.w600,
                     color: Colors.white,
                   ),
                 ),
-                SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
                 Row(
                   children: [
                     Container(
@@ -62,23 +72,22 @@ class UiBaseApp extends StatelessWidget {
                         color: Colors.amber,
                         borderRadius: BorderRadius.circular(45),
                       ),
-                      child: Padding(
+                      child: const Padding(
                         padding: EdgeInsets.symmetric(
                           vertical: 20,
                           horizontal: 50,
                         ),
-                        child: Text(
-                            'Transfer',
-                            style: TextStyle(fontSize: 20,)
-                        ),
+                        child: Text('Transfer',
+                            style: TextStyle(
+                              fontSize: 20,
+                            )),
                       ),
                     )
                   ],
                 )
               ],
             ),
-          )
-      ),
+          )),
     );
   }
 }
