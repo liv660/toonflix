7829d9a596447e1abaff17cca08fbc59a59d9f0f
sysim <liv660n@gmail.com>
2023-05-18 15:10:11 +0900

    ui - add an icon to card

--- lib/ui_challenge/ui_base.dart
+++ lib/ui_challenge/ui_base.dart
@@ -104,6 +104,7 @@ class UiBaseApp extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
+                    clipBehavior: Clip.hardEdge,
                     decoration: BoxDecoration(
                       color: const Color(0xFF1F2123),
                       borderRadius: BorderRadius.circular(25),
@@ -111,6 +112,7 @@ class UiBaseApp extends StatelessWidget {
                     child: Padding(
                       padding: const EdgeInsets.all(20),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -148,6 +150,17 @@ class UiBaseApp extends StatelessWidget {
                                 ],
                               ),
                             ],
+                          ),
+                          Transform.scale(
+                            scale: 2.2,
+                            child: Transform.translate(
+                              offset: const Offset(-5, 12),
+                              child: const Icon(
+                                Icons.euro_rounded,
+                                color: Colors.white,
+                                size: 88,
+                              ),
+                            ),
                           )
                         ],
                       ),
