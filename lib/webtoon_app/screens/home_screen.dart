1dd61ad5fc0e42bbf11b540b24e734ad7d2804ee
sysim <liv660n@gmail.com>
2023-05-23 16:10:37 +0900

    webtoon - add detail screen

--- lib/webtoon_app/screens/home_screen.dart
+++ lib/webtoon_app/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/webtoon_app/services/api_service.dart';
+import 'package:toonflix/webtoon_app/widgets/webtoon_widget.dart';
 
 import '../models/webtoon_model.dart';
 
@@ -52,35 +53,8 @@ class HomeScreen extends StatelessWidget {
       itemBuilder: (context, index) {
         print(index);
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.3))
-                  ]),
-              child: Image.network(
-                webtoon.thumb,
-                headers: const {'Referer': 'https://comic.naver.com'},
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              webtoon.title,
-              style: const TextStyle(
-                fontSize: 22,
-              ),
-            ),
-          ],
-        );
+        return Webtoon(
+            title: webtoon.title, thumb: webtoon.thumb, id: webtoon.id);
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 40,
