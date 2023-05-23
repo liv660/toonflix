86781e401227eebdc9c114112442c1d663cbbdae
sysim <liv660n@gmail.com>
2023-05-23 11:23:53 +0900

    webtoon - fetching data

--- lib/webtoon_app/screens/home_screen.dart
+++ lib/webtoon_app/screens/home_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/webtoon_app/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../models/webtoon_model.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitfForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitfForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
