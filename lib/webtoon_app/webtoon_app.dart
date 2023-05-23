7352546542bcdf49d662fcb7e11fa9c26768e6e1
sysim <liv660n@gmail.com>
2023-05-23 10:25:20 +0900

    webtoon - scaffold

---
+++ lib/webtoon_app/webtoon_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/webtoon_app/screens/home_screen.dart';
+
+class WebtoonApp extends StatelessWidget {
+  const WebtoonApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
